cb7c1298e1019348b477cc6f91f8a2bda3848834
Jannis R <mail@jannisr.de>
2023-05-27 15:19:16 +0200

    parse vessel properties ✅

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const require = createRequire(import.meta.url)
 
 import createDebug from 'debug'
 import {fetch} from 'cross-fetch'
+import {load as loadHtml} from 'cheerio'
+import {parsers as fieldParsers} from './lib/parse.js'
 const pkg = require('./package.json')
 
 const VESSEL_BASE_URL = `\
@@ -43,6 +45,80 @@ const _fetch = async (url, accept, userAgent) => {
 	return {res, body}
 }
 
+const _parseVesselPage = (html) => {
+	const $ = loadHtml(html)
+
+	const paramsTable = $('main .ship-section table.aparams tbody').first()
+	debug('paramsTable', paramsTable)
+	if (!paramsTable || paramsTable.length < 1) {
+		throw new Error('failed to find table.aparams')
+	}
+
+	const rows = $('tr', paramsTable)
+	debug('rows', rows)
+	if (!rows || rows.length < 1) {
+		throw new Error('failed to find table.aparams tr')
+	}
+
+	const ignoredFields = [
+		'Course / Speed', // parsed further below
+	]
+
+	const props = {}
+	for (const tr of rows) {
+		// todo: this doesn't extract "Course / Speed" properly?
+		const key = $('td.n3', tr).text() || null
+		const val = $('td.v3', tr).text() || null
+		if (key === null) {
+			// todo: debug-log
+			continue
+		}
+		if (fieldParsers.has(key)) {
+			const parser = fieldParsers.get(key)
+			const parsed = parser(key, val)
+			Object.assign(props, parsed)
+		} else if (!ignoredFields.includes(key)) {
+			props[key] = val
+		}
+	}
+
+	const mapData = JSON.parse($('#djson').attr('data-json') || '{}')
+	debug('mapData', mapData)
+	// e.g.:
+	// {
+	// 	"v": true,
+	// 	"sar": false,
+	// 	"mmsi": 211627650,
+	// 	"ship_lat": 52.4449,
+	// 	"ship_lon": 13.15839,
+	// 	"ship_cog": 345.0,
+	// 	"ship_sog": 8.0,
+	// 	"ship_type": 69,
+	// 	"no_pc": "No port calls detected"
+	// }
+	// todo: ship_type
+	if (mapData.ship_cog) {
+		props.courseOverGround = parseFloat(mapData.ship_cog) // in degrees
+	}
+	if (mapData.ship_sog) {
+		props.speedOverGround = parseFloat(mapData.ship_sog)
+	}
+	if (mapData.ship_lat) {
+		props.latitude = parseFloat(mapData.ship_lat)
+	}
+	if (mapData.ship_lon) {
+		props.longitude = parseFloat(mapData.ship_lon)
+	}
+
+	// todo: parse recent port calls
+	// todo: parse "vessel utilization"
+	// todo: parse "vessel particulars"
+	// todo: parse "history"
+	// todo: parse "similar vessels"
+
+	return props
+}
+
 const scrapeVesselFromVesselfinder = async (mmsi, opt = {}) => {
 	if ('string' !== typeof mmsi) {
 		throw new TypeError('mmsi must be a string')
@@ -64,11 +140,14 @@ const scrapeVesselFromVesselfinder = async (mmsi, opt = {}) => {
 		target.href,
 		'text/html,application/xhtml+xml;q=0.9',
 		userAgent,
+		'text',
 	)
 
-	// todo
+	const props = _parseVesselPage(body)
+	return props
 }
 
 export {
 	scrapeVesselFromVesselfinder as scrapeVessel,
+	_parseVesselPage,
 }
--- test/index.js
+++ test/index.js
@@ -1,7 +1,105 @@
 import test from 'node:test'
+import {fileURLToPath} from 'node:url'
+import {readFileSync} from 'node:fs'
+import {deepStrictEqual} from 'node:assert'
+import {
+	_parseVesselPage,
+} from '../index.js'
 
-import {todo} from '../index.js'
+const bvgWannseeHtml = readFileSync(
+	fileURLToPath(new URL('./bvg-wannsee-2023-05-27T15%3A35%3A45%2B02%3A00.html', import.meta.url)),
+	{encoding: 'utf8'},
+)
+const containerships6Html = readFileSync(
+	fileURLToPath(new URL('./containerships-6-2023-05-27T15%3A39%3A59%2B02%3A00.html', import.meta.url)),
+	{encoding: 'utf8'},
+)
+const madickenHtml = readFileSync(
+	fileURLToPath(new URL('./madicken-2023-05-27T15%3A43%3A35%2B02%3A00.html', import.meta.url)),
+	{encoding: 'utf8'},
+)
+const costaFavolosaHtml = readFileSync(
+	fileURLToPath(new URL('./costa-favolosa-2023-05-27T15%3A45%3A45%2B02%3A00.html', import.meta.url)),
+	{encoding: 'utf8'},
+)
 
-test('todo', (t) => {
-	// todo
+test('_parseVesselPage works with BVG\'s "Wannsee"', async (t) => {
+	const vessel = await _parseVesselPage(bvgWannseeHtml)
+	deepStrictEqual(vessel, {
+		// todo: distance/time
+		beam: 8,
+		callsign: 'DK4902',
+		courseOverGround: 132.4,
+		currentDraught: null,
+		flag: 'Germany',
+		imo: null,
+		latitude: 52.44495,
+		length: 45,
+		longitude: 13.15466,
+		mmsi: '211627650',
+		navigationStatus: 'under way',
+		positionReceived: '0 min ago',
+		predictedETA: null,
+		speedOverGround: 8.2,
+	})
+})
+test('_parseVesselPage works with "Containerships 6"', async (t) => {
+	const vessel = await _parseVesselPage(containerships6Html)
+	deepStrictEqual(vessel, {
+		// todo: distance/time
+		beam: 22,
+		callsign: 'DABH',
+		courseOverGround: 101,
+		currentDraught: 7.6,
+		flag: 'Germany',
+		imo: '9188518',
+		latitude: 53.541,
+		length: 155,
+		longitude: 9.89333,
+		mmsi: '211315100',
+		navigationStatus: 'under way',
+		positionReceived: '0 min ago',
+		predictedETA: null,
+		speedOverGround: 5.2,
+	})
+})
+test('_parseVesselPage works with "Madicken"', async (t) => {
+	const vessel = await _parseVesselPage(madickenHtml)
+	deepStrictEqual(vessel, {
+		// todo: distance/time
+		beam: 13,
+		callsign: '5BPY5',
+		courseOverGround: 84.8,
+		currentDraught: 5.3,
+		flag: 'Cyprus',
+		imo: '9195755',
+		latitude: 53.83913,
+		length: 89,
+		longitude: 8.88213,
+		mmsi: '210090000',
+		navigationStatus: 'under way',
+		positionReceived: '0 min ago',
+		predictedETA: null,
+		speedOverGround: 10.8,
+	})
+})
+test('_parseVesselPage works with "Costa Favolosa"', async (t) => {
+	const vessel = await _parseVesselPage(costaFavolosaHtml)
+	deepStrictEqual(vessel, {
+		// todo: distance/time
+		beam: 44,
+		callsign: 'ICPK',
+		courseOverGround: 295.1,
+		currentDraught: 8.3,
+		flag: 'Italy',
+		imo: '9479852',
+		latitude: 53.83251,
+		length: 290,
+		longitude: 8.07988,
+		mmsi: '247311100',
+		navigationStatus: 'under way',
+		positionReceived: '0 min ago',
+		predictedETA: null,
+		speedOverGround: 15.9,
+	})
 })
